52d6c6f0be4cd68a73b2097c1c24854d6c688989
blessyyyu <954793264@qq.com>
2021-04-26 20:40:34 +0800

    从数组List中递归构造一棵二叉树

--- BinaryTreeCreate.java
+++ BinaryTreeCreate.java
@@ -1,8 +1,29 @@
 import java.util.*
-
-Class Main{
-    public static void main(String[] args)
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+//      从数组中构建一个二叉树
+class BinaryTreeCreate{
+    /**
+     *
+     * @param list  数组
+     * @param index  需要访问的当前结点
+     * @return 返回根节点
+     */
+    public TreeNode CreateBinaryTree(List<Integer>list, int index)
     {
-        
+        TreeNode curNode = null;
+        if(index < list.size())
+        {
+            int value = list.get(index);
+            curNode = new TreeNode(value);
+            curNode.left = CreateBinaryTree(list,2*index+1);
+            curNode.right = CreateBinaryTree(list,2*index+2);
+            return curNode;
+        }
+        return curNode;
     }
 }
